8aac65e819c14cdc299f22374bea6511fbf3989d
Luiz Augusto <sousaluiz854@gmail.com>
2025-03-17 16:43:09 -0400

    feat: commit de teste atualizacao

--- desafio_01/src/app/app.config.ts
+++ desafio_01/src/app/app.config.ts
@@ -1,9 +1,42 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, Injectable } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+Injectable({
+  providedIn: 'root'
+})
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()]
 };
+
+export class ClienteService {
+  private apiUrl = 'sua-api-url/clientes';
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  get(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  create(cliente: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, cliente);
+  }
+
+  update(cliente: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${cliente.id}`, cliente);
+  }
+
+  delete(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
+}
