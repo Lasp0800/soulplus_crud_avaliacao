c610ae8565feb3050bb2b9d1c4b9f564a56c5cdf
Luiz Augusto <suporte@soulplus.digital>
2025-03-31 18:07:15 -0400

    feat: correcao de codigo

--- desafio_01/src/app/app.component.ts
+++ desafio_01/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
-import { DetalhesClienteComponent } from './detalhes-cliente/detalhes-cliente.component';
+import { ListaClienteComponent } from "./lista-cliente/lista-cliente.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListaClienteComponent, DetalhesClienteComponent],
+  imports: [ListaClienteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  clientes = ['Cliente 1', 'Cliente 2', 'Cliente 3'];
-}
+export class AppComponent {}
